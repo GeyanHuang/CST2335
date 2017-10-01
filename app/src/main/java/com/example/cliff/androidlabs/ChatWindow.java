ff5f9322ffe59a8631d3f29b3b540c419abadef2
cliff <cliff_ok@outlook.com>
2017-10-01 16:05:44 -0400

    Initial commit

--- app/src/main/java/com/example/cliff/androidlabs/ChatWindow.java
+++ app/src/main/java/com/example/cliff/androidlabs/ChatWindow.java
@@ -1,13 +1,77 @@
 package com.example.cliff.androidlabs;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class ChatWindow extends Activity {
+
+    private Button button_send;
+    private EditText edit_message;
+    private ListView list_chat;
+    private ArrayList<String> messages;
+
+    private class ChatAdapter extends ArrayAdapter<String>{
+
+        public ChatAdapter(Context context) {
+            super(context, 0);
+        }
+
+        public int getCount(){
+            return list_chat.getCount();
+        }
+
+        public String getItem(int position){
+            return list_chat.getItemAtPosition(position).toString();
+        }
+
+        public View getView(int position, View convertView, ViewGroup parent){
+            LayoutInflater inflater = ChatWindow.this.getLayoutInflater();
+            View result = null ;
+            if(position%2 == 0)
+                result = inflater.inflate(R.layout.chat_row_incoming, null);
+            else
+                result = inflater.inflate(R.layout.chat_row_outgoing, null);
+            TextView message = result.findViewById(R.id.message_text);
+            message.setText(   getItem(position)  );
+
+
+            return  result;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_window);
+
+
+        button_send = findViewById(R.id.button_send);
+        edit_message = findViewById(R.id.edit_message);
+        list_chat = findViewById(R.id.list_chat);
+        messages = new ArrayList<>();
+
+        final ChatAdapter messageAdapter =new ChatAdapter( this );
+        list_chat.setAdapter (messageAdapter);
+
+
+
+        button_send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                messageAdapter.notifyDataSetChanged();
+                edit_message.setText("");
+            }
+        });
     }
 }
