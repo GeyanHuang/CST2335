b9814a01dc2248d77d0cbdf48ac939434a236a19
cliff <cliff_ok@outlook.com>
2017-10-10 00:55:45 -0400

    Initial commit

---
+++ app/src/main/java/com/example/cliff/androidlabs/ChatDatabaseHelper.java
@@ -0,0 +1,74 @@
+package com.example.cliff.androidlabs;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+/**
+ * Created by cliff on 2017-10-09.
+ */
+
+public class ChatDatabaseHelper extends SQLiteOpenHelper {
+
+    private static final String LOGTAG = ChatDatabaseHelper.class.getSimpleName();
+
+    public static final String DATABASE_NAME = "Messages.db";
+    public static final int DATABASE_VERSION = 1;
+
+    public static final String TABLE_NAME = "Messages";
+    public static final String COLUMN_ID = "MessageID";
+    public static final String COLUMN_CONTENT = "Message";
+
+    private SQLiteDatabase database;
+
+    private static final String TABKE_CREATE = "CREATE TABLE " + TABLE_NAME + " (" +
+            COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+            COLUMN_CONTENT + " TEXT" +
+            ")";
+
+    public ChatDatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL(TABKE_CREATE);
+        Log.i("ChatDatabaseHelper", "Calling onCreate");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(sqLiteDatabase);
+        Log.i("ChatDatabaseHelper", "Calling onUpgrade, oldVersion=" + i + " newVersion=" + i1);
+    }
+
+    public void openDatabase() {
+        database = getWritableDatabase();
+    }
+
+    public void closeDatabase() {
+        if (database != null && database.isOpen()){
+            database.close();
+        }
+    }
+
+    public long insert(String content){
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_CONTENT, content);
+
+        return database.insert(TABLE_NAME, null, values);
+
+    }
+
+    public void deleteAll(){
+        getWritableDatabase().execSQL("delete from "+ TABLE_NAME);
+    }
+
+    public Cursor getAllRecords(){
+        return database.query(TABLE_NAME,null,null,null,null,null,null);
+    }
+}
--- app/src/main/java/com/example/cliff/androidlabs/ChatWindow.java
+++ app/src/main/java/com/example/cliff/androidlabs/ChatWindow.java
@@ -2,7 +2,9 @@ package com.example.cliff.androidlabs;
 
 import android.app.Activity;
 import android.content.Context;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,78 +13,125 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class ChatWindow extends Activity {
 
+    ChatDatabaseHelper databaseHelper;
+    private String TAG = ChatWindow.class.getSimpleName();
+
     private Button button_send;
     private EditText edit_message;
     private ListView list_chat;
     private ArrayList<String> messages;
+    private ChatAdapter messageAdapter;
+
+    class ChatAdapter extends ArrayAdapter<String> {
+
+        public ChatAdapter(Context context) {
+            super(context, 0);
+        }
+
+        public String getItem(int position) {
+            return messages.get(position);
+        }
+
+        @Override
+        public int getCount() {
+
+            int count = messages.size();
+            return count;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            LayoutInflater inflater = ChatWindow.this.getLayoutInflater();
+
+            View result;
+            if (position % 2 == 0)
+                result = inflater.inflate(R.layout.chat_row_incoming, null);
+            else
+                result = inflater.inflate(R.layout.chat_row_outgoing, null);
+
+            TextView message = result.findViewById(R.id.message_text);
+
+            message.setText(getItem(position));
+
+            return result;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_window);
 
+        init();
 
-        button_send = findViewById(R.id.button_send);
-        edit_message = findViewById(R.id.edit_message);
-        list_chat = findViewById(R.id.list_chat);
-        messages = new ArrayList<>();
-
-        class ChatAdapter extends ArrayAdapter<String> {
-
-            public ChatAdapter(Context context, ArrayList<String> messages) {
-                super(context, 0, messages);
-            }
-
-            public String getItem(int position){
-                return messages.get(position);
-            }
-
-            @Override
-            public int getCount() {
-
-                int count = messages.size();
-                return count;
-            }
-
-            @Override
-            public View getView(int position, View convertView, ViewGroup parent) {
-
-                LayoutInflater inflater = ChatWindow.this.getLayoutInflater();
-
-                View result;
-                if (position % 2 == 0)
-                    result = inflater.inflate(R.layout.chat_row_incoming, null);
-                else
-                    result = inflater.inflate(R.layout.chat_row_outgoing, null);
-
-                TextView message = result.findViewById(R.id.message_text);
-
-                message.setText(getItem(position));
-
-                return result;
-            }
-        }
-
-
-        final ChatAdapter messageAdapter = new ChatAdapter(this,messages);
         list_chat.setAdapter(messageAdapter);
+        registerForContextMenu(list_chat);
 
         button_send.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if (!edit_message.getText().toString().equals("")) {
-                    messages.add(edit_message.getText().toString());
-                    messageAdapter.notifyDataSetChanged();
+                    databaseHelper.insert(edit_message.getText().toString());
+                    showHistory();
                     edit_message.setText("");
-                    scrollMyListViewToBottom();
                 }
             }
         });
+
+        button_send.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                databaseHelper.deleteAll();
+                showHistory();
+                Toast.makeText(ChatWindow.this,R.string.delete_history,Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+    }
+
+    public void init() {
+        //init
+        button_send = findViewById(R.id.button_send);
+        edit_message = findViewById(R.id.edit_message);
+        list_chat = findViewById(R.id.list_chat);
+        databaseHelper = new ChatDatabaseHelper(this);
+        messages = new ArrayList<>();
+        messageAdapter = new ChatAdapter(this);
+    }
+
+    public void showHistory() {
+        messages.clear();
+        Cursor cursor = databaseHelper.getAllRecords();
+        for (cursor.moveToFirst(); !cursor.isAfterLast(); cursor.moveToNext()) {
+            messages.add(cursor.getString(cursor.getColumnIndex(databaseHelper.COLUMN_CONTENT)));
+            Log.i(TAG, "SQL MESSAGE:" + cursor.getString( cursor.getColumnIndex( ChatDatabaseHelper.COLUMN_CONTENT) ) );
+        }
+        messageAdapter.notifyDataSetChanged();
+        scrollMyListViewToBottom();
+        Log.i(TAG, "Cursor’s  column count = " + cursor.getColumnCount() );
+        for (int i = 0; i < cursor.getColumnCount(); i++){
+            Log.i(TAG, "Cursor’s  column name = " + (i + 1) + ". " + cursor.getColumnName(i) );
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        databaseHelper.openDatabase();
+        showHistory();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        databaseHelper.closeDatabase();
     }
 
     private void scrollMyListViewToBottom() {
