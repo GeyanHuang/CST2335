0401fdf678c8212a8f710467aa9a3cfa247fe731
cliff <cliff_ok@outlook.com>
2017-11-20 12:22:56 -0500

    Initial commit

--- app/src/main/java/com/example/cliff/androidlabs/WeatherForecast.java
+++ app/src/main/java/com/example/cliff/androidlabs/WeatherForecast.java
@@ -2,12 +2,19 @@ package com.example.cliff.androidlabs;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
 
 public class WeatherForecast extends Activity {
+ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather_forecast);
+
+        progressBar = findViewById(R.id.progressbar);
+
+        progressBar.setVisibility(View.VISIBLE);
     }
 }
