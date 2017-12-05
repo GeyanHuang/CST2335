83a6dea5c7fc805473dd0bbb2e3c0b68b0681a9c
cliff <cliff_ok@outlook.com>
2017-12-05 14:39:26 -0500

    Initial commit

--- app/src/main/java/com/example/cliff/androidlabs/TestToolbar.java
+++ app/src/main/java/com/example/cliff/androidlabs/TestToolbar.java
@@ -1,20 +1,27 @@
 package com.example.cliff.androidlabs;
 
-import android.content.DialogInterface;
+import android.app.Dialog;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.WindowManager;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class TestToolbar extends AppCompatActivity {
+
+    private Dialog customDialog;
+    private EditText editText_message;
+    private String message;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +29,10 @@ public class TestToolbar extends AppCompatActivity {
         setContentView(R.layout.activity_test_toolbar);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        SharedPreferences sharedPref = getSharedPreferences("User info", Context.MODE_PRIVATE);
+        message = sharedPref.getString("message", "");
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -44,27 +55,23 @@ public class TestToolbar extends AppCompatActivity {
         switch (item.getItemId()) {
             case R.id.action_one:
                 Log.d("Toolbar", "Option 1 selected");
-                Snackbar.make(this.findViewById(R.id.action_one), R.string.item_1, Snackbar.LENGTH_LONG)
+                Snackbar.make(this.findViewById(R.id.action_one),message, Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
                 break;
             case R.id.action_two:
                 Snackbar.make(this.findViewById(R.id.action_two), R.string.item_2, Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
-                AlertDialog.Builder builder = new AlertDialog.Builder(TestToolbar.this);
-                builder.setTitle(R.string.dialog_message);
-                builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        finish();
-                    }
-                });
-                builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
+                android.app.AlertDialog.Builder commentBuilder = new android.app.AlertDialog.Builder(this);
+                View commentView = getLayoutInflater().inflate(R.layout.custom_dialog, null);
+                editText_message = commentView.findViewById(R.id.editText_message);
+                editText_message.setText(editText_message.getText());
 
-                    }
-                });
-                AlertDialog dialog = builder.create();
-                dialog.show();
-
+                commentBuilder.setView(commentView);
+                customDialog = commentBuilder.create();
+                customDialog.setCanceledOnTouchOutside(false);
+                customDialog.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
+                customDialog.show();
+                editText_message.requestFocus();
                 break;
             case R.id.action_three:
                 Snackbar.make(this.findViewById(R.id.action_three), R.string.item_3, Snackbar.LENGTH_LONG)
@@ -77,5 +84,16 @@ public class TestToolbar extends AppCompatActivity {
                 break;
         }
         return true;
+    }
+
+    public void message_check(View view) {
+        message = editText_message.getText().toString();
+
+        SharedPreferences sharedPref = getSharedPreferences("User info", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putString("message", message);
+        editor.apply();
+
+        customDialog.dismiss();
     }
 }
